f9fa08057f17140d6729378443575575935d0082
unknown <miner717@hotmail.com>
2026-02-28 00:50:17 +0000

    Initial commit

---
+++ BetterEnemies/Utilities/Logging.cs
@@ -0,0 +1,27 @@
+﻿namespace BetterEnemies.Utilities
+{
+	internal static class Logging
+	{
+		public static void Log(string message, TLDLoader.Logger.LogLevel logLevel = TLDLoader.Logger.LogLevel.Info) =>
+			BetterEnemies.I.Logger.Log(message, logLevel);
+
+		public static void LogDebug(string message)
+		{
+#if DEBUG
+			BetterEnemies.I.Logger.LogDebug(message);
+#endif
+		}
+
+		public static void LogInfo(string message) =>
+			BetterEnemies.I.Logger.LogInfo(message);
+
+		public static void LogWarning(string message) =>
+			BetterEnemies.I.Logger.LogWarning(message);
+
+		public static void LogError(string message) =>
+			BetterEnemies.I.Logger.LogError(message);
+
+		public static void LogCritical(string message) =>
+			BetterEnemies.I.Logger.LogCritical(message);
+	}
+}
